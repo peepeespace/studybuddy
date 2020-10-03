8e6d81a59f8e7e2da7247a0cc4885f441709d8ab
peepee <ppark9553@gmail.com>
2020-10-03 21:19:50 +0900

    event popup + 선생님 카드 클릭시 이동

--- assets/js/index.js
+++ assets/js/index.js
@@ -12,6 +12,40 @@ logo.addEventListener("click", (e) => {
   window.location = "/";
 });
 
+const opicPopup = document.getElementById("opic-popup");
+const opicClose = document.getElementById("opic-close");
+const opicEvent = document.getElementById("opic-event");
+const webPopup = document.getElementById("web-popup");
+const webClose = document.getElementById("web-close");
+const webEvent = document.getElementById("web-event");
+
+opicClose.addEventListener("click", (e) => {
+  opicPopup.style.display = "none";
+});
+
+opicEvent.addEventListener("click", (e) => {
+  window.location = "/main-english";
+});
+
+webClose.addEventListener("click", (e) => {
+  webPopup.style.display = "none";
+});
+
+webEvent.addEventListener("click", (e) => {
+  window.location = "/main-coding";
+});
+
+const anne = document.getElementsByClassName("anne")[0];
+const phillip = document.getElementsByClassName("phillip")[0];
+
+anne.addEventListener("click", (e) => {
+  window.location = "/english";
+});
+
+phillip.addEventListener("click", (e) => {
+  window.location = "/coding";
+});
+
 const messageAPI = "https://api.studybuddy.kr/core/message/";
 
 const messageForm = document.getElementById("message-form");
